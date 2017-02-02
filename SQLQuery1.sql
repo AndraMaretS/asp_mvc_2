19efb53af84a9acbad20d5fde5bfde7e9ff46dc3
Andra Maret <andramaret4@gmail.com>
2017-02-02 07:13:36 +0700

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SYSUserRole](
+ [SYSUserRoleID] [int] IDENTITY(1,1) NOT NULL,
+ [SYSUserID] [int] NOT NULL,
+ [LOOKUPRoleID] [int] NOT NULL,
+ [IsActive] [bit] DEFAULT (1),
+ [RowCreatedSYSUserID] [int] NOT NULL,
+ [RowCreatedDateTime] [datetime] DEFAULT GETDATE(),
+ [RowModifiedSYSUserID] [int] NOT NULL,
+ [RowModifiedDateTime] [datetime] DEFAULT GETDATE(),
+ PRIMARY KEY (SYSUserRoleID)
+)
